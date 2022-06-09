ac74a23622019aeccbdbb000e9f0e4e2379d5aa2
isntkyu <jkjkjk5710@gmail.com>
2022-06-10 01:38:01 +0900

    init app

---
+++ README.md
@@ -0,0 +1,2 @@
+로컬 디비 파일 RDS 업로드 모듈
+https://github.com/tryghost/node-sqlite3/wiki/Control-Flow
---
+++ app.js
@@ -0,0 +1,38 @@
+const sqlite3 = require("sqlite3").verbose();
+
+// open database in memory
+let db = new sqlite3.Database("C:/Users/jk/Downloads/Asset.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the chinook database.");
+});
+
+// 전체 테이블 명 조회
+let sql = `SELECT name FROM sqlite_master WHERE type IN ('table', 'view') AND name NOT LIKE 'sqlite_%' UNION ALL SELECT name FROM sqlite_temp_master WHERE type IN ('table', 'view') ORDER BY 1;`;
+
+let sql1 = `select * from assets;`;
+let sql2 = `select * from rentHistories;`;
+let sql3 = `select * from returnHistories;`;
+let sql4 = `select * from users;`;
+
+db.serialize(() => {
+  //직렬
+  db.all(sql1, [], (err, rows) => {
+    // 모든결과
+    if (err) {
+      throw err;
+    }
+    // rows.forEach((row) => {
+    console.log(rows);
+    // });
+  });
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
+});
