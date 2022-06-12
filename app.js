2296d1a3a0a57d034f6268e6322ffff4d4f18a7a
이준규 <jkjkjk5710@gmail.com>
2022-06-12 20:38:50 +0900

    var -> const

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ async function mappingReturnArray(rows) {
 }
 
 function timestamp() {
-  var today = new Date();
+  const today = new Date();
   today.setHours(today.getHours() + 9);
   return today.toISOString().replace("T", " ").substring(0, 19);
 }
